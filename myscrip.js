24b86ae663dadd3f119c9dd51806c1e890b246fc
LaraDeadman <149664230+LaraDeadman@users.noreply.github.com>
2023-11-02 02:43:17 -0600

    Add files via upload

---
+++ myscrip.js
@@ -0,0 +1,15 @@
+<script> 
+      var swiper = new swipper (".myswipper", {
+        effect: "coverflow",
+        grabcursor: true,
+        centeredslides: true,
+        slidesPerView: "auto",
+        loop: true,
+        coverFlowEffect: {
+           depht: 500,
+           modifer:1,
+           slidesShadows: true,
+         }
+    })
+
+   </script>
